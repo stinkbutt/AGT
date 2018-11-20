5cd658e65cd96cc8e96a6e4321312f3c4e42a861
stinkbutt <>
2018-11-20 15:27:27 -0500

    added URI query strings, bus/train option that changes ticket expiry time

--- bin/agtransit.js
+++ bin/agtransit.js
@@ -1,17 +1,28 @@
 var date = new Date();
 //var n = date.toDateString();
 var start = date.getTime();
-var duration = 10800000; // 3 hours in ms
-var end = start + duration;
-
-
+//var duration = 9900000; // 2:45 hours in ms
+var duration = -1; // 2:45 hours in ms
 
 var line1string = "line1";
 var line2string = "line2";
 
 var elem = document.documentElement;
 
-  
+var color = getQueryVariable("color");
+var mode = getQueryVariable("mode");
+var orig = getQueryVariable("orig");
+var dest = getQueryVariable("dest");
+
+if (mode == "train"){
+  duration = 9900000; // 2:45 hours for train
+  } else {
+      duration = 3600000; // 1:00 hour for bus
+    }
+
+var end = start + duration;
+
+console.log(duration / 60000, "Orig: " + orig + " Dest: " + dest);
 
 function openFullscreen() {
   if (elem.requestFullscreen) {
@@ -34,6 +45,29 @@ function closeFullscreen() {
     document.webkitExitFullscreen();
   } else if (document.msExitFullscreen) {
     document.msExitFullscreen();
+  }
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+
+function getURIargument(args) {
+  var parsedUrl = new URL(window.location.href);
+  var args = parsedUrl.search.split("?" + args + "=");
+  //console.log(parsedUrl.search.split("?color="));
+  if (args[1] == undefined){
+    return "undefined";
+  } else {
+      return args[1];
   }
 }
 
@@ -162,6 +196,7 @@ function msToTime(s) {
 }
 
 function startIt() {
+    console.log("img/top_" + color + ".png" + " " + "img/bar_" + color + ".png");
     setInterval(myFunction, 1000);
 }
 
@@ -169,10 +204,13 @@ function myFunction(){
 	  line1string =  displayTime(); 
     line2string = day + ", " + month + " " + date.getDate() + ", " + date.getFullYear();
 	  //line3string = "Expires in " + timeLeft;
-   
-	document.getElementById("line3").innerHTML = line3string;
+  document.getElementById("top").src = "img/top_" + color + ".png";
+  document.getElementById("bar").src = "img/bar_" + color + ".png";
+ 
 	document.getElementById("line1").innerHTML = line1string;
 	document.getElementById("line2").innerHTML = line2string;
+  document.getElementById("line3").innerHTML = line3string;
+
 }
 
 
